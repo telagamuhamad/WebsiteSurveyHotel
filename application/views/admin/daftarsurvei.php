45ebc660a4e84a644f7fbd26e6898f1186cb9b3c
Dian Asmara Dahana <dahonobambanghandoko@gmail.com>
2020-07-19 06:33:23 -0700

    Tampilan Daftar Survei
    Tampilan Daftar Survei

--- application/views/admin/daftarsurvei.php
+++ application/views/admin/daftarsurvei.php
@@ -1,87 +1,143 @@
-      <div class="title-page text-center" style="color: black; font-family: viga; padding-bottom: 30px; padding-top: 1px;">
-            <h1>Daftar Survei</h1>
-        </div>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-        <!-- DataTables -->
-        <div class="card" style="box-shadow: 3px 3px 15px blue;">
-<!-- DataTales Example -->
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-          <div class="card shadow" >
-            <div class="card-body">
-              <div class="row" >
-                 <?= $this->session->flashdata('message'); ?>
-              </div>
-              <div class="row">
-             <div class="col-md-5 " style=" margin-top: 2px; margin-left: 58%;font-family: Viga;">
-            <form action="<?= base_url('c_admin_daftarsurvei') ?>" method="post">
-              <div class="input-group mb-3">
-                <input style="font-family: Viga;" type="text" class="form-control" placeholder="Klik submit untuk Pencarian/Refresh" name="cariberkas" autocomplete="off" autofocus>
-                <div class="input-group-append">
-                  <input style="font-family: Viga;" class="btn btn-primary" type="submit" name="submit">
-                </div>
-              </div>
-            </form>
+    <div class="card-deck">
+  <div class="card">
+    <img class="card-img-top" src="assets/image/1-Januari.jpg" alt="Card image cap">
+    <div class="card-body">
+      <a href="#" class="btn btn-primary">LIHAT</a>
+    </div>
+    <div class="card-footer">
+    </div>
+  </div>
+  <div class="card">
+    <img class="card-img-top" src="assets/image/2-Februari.jpg" alt="Card image cap">
+    <div class="card-body">
+      <a href="#" class="btn btn-primary">LIHAT</a>
+    </div>
+    <div class="card-footer">
+    </div>
+  </div>
+  <div class="card"style="margin-right: 5px">
+    <img class="card-img-top" src="assets/image/3-Maret.jpg" alt="Card image cap">
+    <div class="card-body">
+      <a href="#" class="btn btn-primary">LIHAT</a>
+    </div>
+    <div class="card-footer">
+  </div>
+  </div>
+  <br/br>
+  <div class="card-deck" style="margin-left: 0.1px">
+  <div class="card">
+    <img class="card-img-top" src="assets/image/4-April.jpg" alt="Card image cap">
+    <div class="card-body">
+      <a href="#" class="btn btn-primary">LIHAT</a>
+    </div>
+    <div class="card-footer">
+    </div>
+  </div>
+  <div class="card" style="margin-left: 1.5px">
+    <img class="card-img-top" src="assets/image/5-Mei.jpg" alt="Card image cap">
+    <div class="card-body">
+      <a href="#" class="btn btn-primary">LIHAT</a>
+    </div>
+    <div class="card-footer">
+    </div>
+  </div>
+  <div class="card">
+    <img class="card-img-top" src="assets/image/6-Juni.jpg" alt="Card image cap">
+    <div class="card-body">
+      <a href="#" class="btn btn-primary">LIHAT</a>
+    </div>
+    <div class="card-footer">
+  </div>
+</div>
+<br/br>
+<div class="card-deck" style="margin-left: 0.1px">
+  <div class="card">
+    <img class="card-img-top" src="assets/image/7-Juli.jpg" alt="Card image cap">
+    <div class="card-body">
+      <a href="#" class="btn btn-primary">LIHAT</a>
+    </div>
+    <div class="card-footer">
+    </div>
+  </div>
+  <div class="card" style="margin-left: 1.5px">
+    <img class="card-img-top" src="assets/image/8-Agustus.jpg" alt="Card image cap">
+    <div class="card-body">
+      <a href="#" class="btn btn-primary">LIHAT</a>
+    </div>
+    <div class="card-footer">
+    </div>
+  </div>
+  <div class="card" style="margin-left: 5px">
+    <img class="card-img-top" src="assets/image/9-September.jpg" alt="Card image cap">
+    <div class="card-body">
+      <a href="#" class="btn btn-primary">LIHAT</a>
+    </div>
+    <div class="card-footer">
+  </div>
+</div>
+<br/br>
+<div class="card-deck" style="margin-left: 0.1px">
+  <div class="card">
+    <img class="card-img-top" src="assets/image/10-Oktober.jpg" alt="Card image cap">
+    <div class="card-body">
+      <a href="#" class="btn btn-primary">LIHAT</a>
+    </div>
+    <div class="card-footer">
+    </div>
+  </div>
+  <div class="card" style="margin-left: 5px">
+    <img class="card-img-top" src="assets/image/11-November.jpg" alt="Card image cap">
+    <div class="card-body">
+      <a href="#" class="btn btn-primary">LIHAT</a>
+    </div>
+    <div class="card-footer">
+    </div>
+  </div>
+  <div class="card" style="margin-left: 8px">
+    <img class="card-img-top" src="assets/image/12-Desember.jpg" alt="Card image cap">
+    <div class="card-body">
+      <a href="#" class="btn btn-primary">LIHAT</a>
+    </div>
+    <div class="card-footer">
+  </div>
+</div>
+</div>
+</div>
+</div>
+
+      <!-- Footer -->
+      <!-- <footer class="footer " style=" margin-top: 250px; bottom: 0;">
+        <div class="container "style=" box-shadow: 0px -3px 5px 0px rgba(0,0,0,5); margin-top: 100px;">
+          <div class="copyright text-center" style="color: black; font-family: Viga; background-color:#DCDCDC; padding-top: 15px; padding-bottom:15px;">
+            <h7>Badan Pusat Statistik Kota Bandar Lampung</h7>
+            <br>
+            <h7>Jl. Sutan Syahrir No. 30, Pahoman, Bandar Lampung, 35215.</h7>
+            <h7>Telp. (0721)255980. Mailbox : bps1871@bps.go.id</h7>
+            <br>
+            <h7>Copyright © 2019</h7>
+            <br>
+            <h7>Built by © Mahasiswa Kerja Praktik <a href="http://if.itera.ac.id/">Prodi Teknik Informatika - <a href="https://www.itera.ac.id/">Institut Teknologi Sumatera</h7>
           </div>
-        
-              <div class="table-responsive">
-                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0" >
-                  <thead class="text-center"style="background-color: #4169E1;">
-                    <tr style="color: white;font-family: Viga;">
-                    <th >Tahun</th>
-                    <th >Bulan</th>
-                    <th >Nama Pengirim</th>
-                    <th >Asal Hotel</th>
-                    <th >Dokumen</th>
-                    <th >Aksi</th>
-                  </tr>
-                  </thead>
-                
-                  <tbody>
-                   <?php if (empty($data_survei)) : ?>
-                      <tr>
-                        <td colspan="6">
-                          <div style="font-family: Viga;" class="alert alert-danger" role="alert">
-                            Data tidak ditemukan!
-                          </div>
-                        </td>
-                      </tr>
-                    <?php endif; ?>
-                  <?php foreach ($data_survei as $data_survei): ?>
-                  <tr style="color: black;font-family: Viga; text-align: center;">
-                    <td >
-                      <?= $data_survei->tahun ?>
-                    </td>
-                    <td>
-                      <?= $data_survei->bulan ?>
-                    </td>
-                    <td>
-                      <?= $data_survei->pengirim ?>
-                    </td>
-                    <td>
-                      <?= $data_survei->asal_hotel ?>
-                    </td>
-                    <td>
-                      <?= $data_survei->file ?>
-                    </td>
-                    
-                    <td width="225px;">
-                      <a href="<?=  base_url('c_admin_daftarsurvei/delete/') . $data_survei->id_berkas ?>" class="btn btn-small text-danger"><i class="fas fa-fw fa-trash"></i> Hapus</a>
-                     <a href="<?=  base_url('assets/berkas'); ?>/<?= $data_survei->file ?>" data-toggle="tooltip" class="btn btn-small text-primary" target="_blank" ><i class="fas fa-fw fa-download"></i> Unduh</a>
-                    </td>
-
-                  </tr>
-                  <?php endforeach; ?>
-                </tbody>
-                
-                </table>
-
-                 <?= $this->pagination->create_links(); ?> 
-
-              </div>
-            </div>
-          </div>
         </div>
-      
-      <!-- /.container-fluid -->
- 
-       
+      </footer> -->
+      <!-- End of Footer -->
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
